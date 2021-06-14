224d15d50019d726dc772866f95502093091f94e
VRez-web <vit.79@bk.ru>
2021-06-14 15:19:40 +0300

    add new routes and dinamic titles for pages

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,41 @@ const routes = [
     path: "/",
     name: "home",
     component: () => import("@/views/Home.vue"),
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/blogs",
     name: "blogs",
     component: () => import("@/views/Blogs.vue"),
+    meta: {
+      title: "Blogs",
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login.vue"),
+    meta: {
+      title: "Login",
+    },
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("@/views/Register.vue"),
+    meta: {
+      title: "Register",
+    },
+  },
+  {
+    path: "/forgot-password",
+    name: "forgotPassword",
+    component: () => import("@/views/ForgotPassword.vue"),
+    meta: {
+      title: "Forgot password",
+    },
   },
 ];
 
@@ -20,6 +50,12 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | FireBlog`;
+
+  next();
 });
 
 export default router;
